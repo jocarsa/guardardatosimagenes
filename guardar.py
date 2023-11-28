cbb73cc77510a580218b6645c3640990d3ca5367
jocarsa <info@josevicentecarratala.com>
2023-11-28 20:20:23 +0100

    3

--- guardar.py
+++ guardar.py
@@ -1,9 +1,15 @@
 from PIL import Image
+import random
 
 imagen = Image.open("imagen.png")
-
+anchura = 512
+altura = 512
 pixeles = imagen.load()
-
-pixeles[0,0] = (255,0,0)
+for x in range(0,anchura):
+    for y in range(0,altura):
+        rojo = random.randint(0,255)
+        verde = random.randint(0,255)
+        azul = random.randint(0,255)
+        pixeles[x,y] = (rojo,verde,azul)
 
 imagen.save("imagen2.png")
