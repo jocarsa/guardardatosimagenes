63e019f0936fb57f2417d11599a285bb5c92ec05
jocarsa <info@josevicentecarratala.com>
2023-11-28 20:44:09 +0100

    8

--- leer.py
+++ leer.py
@@ -6,14 +6,17 @@ anchura = 512
 altura = 512
 pixeles = imagen.load()
 
-for x in range(0,512):
-    color = pixeles[x,0]
-    rojo = color[0]
-    verde = color[1]
-    azul = color[2]
-    print(chr(rojo))
-    print(chr(verde))
-    print(chr(azul))
+
+for y in range(0,512):
+    for x in range(0,512):
+
+        color = pixeles[x,y]
+        rojo = color[0]
+        verde = color[1]
+        azul = color[2]
+        print(chr(rojo), end='')
+        print(chr(verde), end='')
+        print(chr(azul), end='')
 
 
 
