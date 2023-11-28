c7c642f3b2d8bb87d8c505537b8feed2fb493677
jocarsa <info@josevicentecarratala.com>
2023-11-28 20:24:26 +0100

    3

--- guardar.py
+++ guardar.py
@@ -5,11 +5,11 @@ imagen = Image.open("imagen.png")
 anchura = 512
 altura = 512
 pixeles = imagen.load()
-for x in range(0,anchura):
-    for y in range(0,altura):
-        rojo = random.randint(0,255)
-        verde = random.randint(0,255)
-        azul = random.randint(0,255)
-        pixeles[x,y] = (rojo,verde,azul)
 
-imagen.save("imagen2.png")
+cadena = "Hola que tal como estais"
+contador = 0
+for caracter in cadena:
+    print(caracter)
+
+
+
